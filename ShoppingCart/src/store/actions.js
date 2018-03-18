633f85f1a737ed3fa8e3888c439778c33a2f796e
Elijah <elijahsharper1@gmail.com>
2018-03-18 12:57:53 +0300

    Actions and mutations

--- ShoppingCart/src/store/actions.js
+++ ShoppingCart/src/store/actions.js
@@ -1,4 +1,13 @@
+import axios from 'axios'
+
 // get products
+export const getProducts = ({ commit }) => {
+  return axios.get('http://cartapi.test/api/products').then((response) => {
+    commit('setProducts', response.data)
+    return Promise.resolve()
+  })
+}
+
 // get cart
 // add a product to our cart
 // remove a product from our cart
